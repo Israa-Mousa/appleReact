39845215228b791a750d3bda91f981ba5402d0c0
Israa Mousa <israa.mazaraa@yahoo.com>
2022-05-17 20:13:55 +0200

    first commit2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,7 @@
 import React from 'react'
 
 const Header = () => {
-    const selectElement=(element)=>document.querySelector(element);
-selectElement('.mobile-menu').addEventListener('click',()=>{
-    console.log('sjakhsahdj');
-
-selectElement('header').classList.toggle('active');
-
-});
+ 
   return (
       <header>
     <div>
